d8ce4642e76630cc319623ae19dac477a3196621
dafma <mar_tp@hotmail.es>
2017-02-09 17:21:26 -0600

    creado la vista de detalle contacto  y list view en el mainactivityPrincipal

---
+++ app/src/main/java/com/example/seader/miscontactos/Contacto.java
@@ -0,0 +1,47 @@
+package com.example.seader.miscontactos;
+
+/**
+ * Created by seader on 9/02/17.
+ */
+
+public class Contacto {
+
+
+
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Contacto(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/seader/miscontactos/MainActivity.java
+++ app/src/main/java/com/example/seader/miscontactos/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.seader.miscontactos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contacto> contactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactos = new ArrayList<Contacto>();
+
+        contactos.add(new Contacto("Anahi Salgado", "725896474", "jas@gmail.com"));
+        contactos.add(new Contacto("Pedro Salgado", "568888822", "fd@gmail.com"));
+        contactos.add(new Contacto("Daniel Rivas", "555896474", "reads@gmail.com"));
+        contactos.add(new Contacto("Arnulfo Martinez", "725896474", "cas@gmail.com"));
+        contactos.add(new Contacto("Juan Colin", "345896474", "tyu@gmail.com"));
+
+
+        ArrayList<String> nombresContacto = new ArrayList<>();
+        for(Contacto contacto: contactos){
+            nombresContacto.add(contacto.getNombre());
+        }
+
+        ListView lstContactos = (ListView) findViewById(R.id.lstContactos);
+        lstContactos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombresContacto));
     }
 }
