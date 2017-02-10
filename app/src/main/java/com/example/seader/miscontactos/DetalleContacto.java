263837f31277ec049d40bf94a79354849004cdd4
dafma <mar_tp@hotmail.es>
2017-02-09 20:15:14 -0600

    agregado accion a los botones llamada e email

--- app/src/main/java/com/example/seader/miscontactos/DetalleContacto.java
+++ app/src/main/java/com/example/seader/miscontactos/DetalleContacto.java
@@ -1,10 +1,20 @@
 package com.example.seader.miscontactos;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetalleContacto extends AppCompatActivity {
+
+    private TextView tvNombre;
+    private TextView tvTelefono;
+    private TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +23,44 @@ public class DetalleContacto extends AppCompatActivity {
 
         Bundle parametros = getIntent().getExtras();
 
-        String nombre   = parametros.getString(getResources().getString(R.string.pnombre));
+        String nombre = parametros.getString(getResources().getString(R.string.pnombre));
         String telefono = parametros.getString(getResources().getString(R.string.ptelefono));
-        String email    = parametros.getString(getResources().getString(R.string.pemail));
+        String email = parametros.getString(getResources().getString(R.string.pemail));
 
-        TextView tvNombre   = (TextView) findViewById(R.id.tvNombre);
-        TextView tvTelefono = (TextView) findViewById(R.id.tvTelefono);
-        TextView tvEmail    = (TextView) findViewById(R.id.tvEmail);
+        tvNombre = (TextView) findViewById(R.id.tvNombre);
+        tvTelefono = (TextView) findViewById(R.id.tvTelefono);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
 
         tvNombre.setText(nombre);
         tvTelefono.setText(telefono);
         tvEmail.setText(email);
+    }
+
+    //alt enter para que automaticamente genere el codigo y no hace mas que verificar si el usuario ha dado el permiso
+    public void llamar(View v) {
+        String telefono = tvTelefono.getText().toString();
+        //codigo automatico
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + telefono)));
+    }
+
+    public void enviarMail(View v){
+        String email = tvEmail.getText().toString();
+        Intent emailIntent = new Intent((Intent.ACTION_SEND));
+        emailIntent.setData(Uri.parse("mailto"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, email);
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Email"));
+
+
     }
 }
